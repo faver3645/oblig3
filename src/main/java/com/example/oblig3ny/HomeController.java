e2f28e336e4796778670fec36204e00bed56eef0
faver3645 <143396926+faver3645@users.noreply.github.com>
2024-04-18 13:38:30 +0200

    first commit

--- src/main/java/com/example/oblig3ny/HomeController.java
+++ src/main/java/com/example/oblig3ny/HomeController.java
@@ -20,40 +20,12 @@ public class HomeController {
     @Autowired
     private BillettRepository rep;
 
-    Logger logger = LoggerFactory.getLogger(HomeController.class);
 
-    private boolean validerBillett(Billett billett){
-        String regexFilm = "[a-zA-ZæøåÆØÅ. \\-]{2,10}";
-        String regexAntall = "[1-9]\\d*";
-        String regexFornavn = "[A-ZÆØÅa-zæøå. -]{2,50}";
-        String regexEtternavn = "[A-ZÆØÅa-zæøå. -]{2,50}";
-        String regexTelefonnr = "[0-9]{8}";
-        String regexEpost = "[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\\.[A-Za-z]{2,}";
-        boolean filmOK = billett.getFilm().matches(regexFilm);
-        String antallString = String.valueOf(billett.getAntall());
-        boolean antallOK = antallString.matches(regexAntall);
-        boolean fornavnOK = billett.getFornavn().matches(regexFornavn);
-        boolean etternavnOk = billett.getEtternavn().matches(regexEtternavn);
-        boolean telefonnrOK = billett.getTelefonnr().matches(regexTelefonnr);
-        boolean epostOK = billett.getEpost().matches(regexEpost);
-        if(filmOK && antallOK && fornavnOK && etternavnOk && telefonnrOK && epostOK){
-            return true;
-        }
-        else {
-            logger.error("Valideringsfeil!");
-            return false;
-        }
-    }
 
     @PostMapping("/lagre")
-    public void lagreBillett(Billett innBillett, HttpServletResponse response) throws IOException {
-        if(!validerBillett(innBillett)){
-            response.sendError(HttpStatus.NOT_ACCEPTABLE.value(),"Valideringsfeil - prøv igjen");
+    public void lagreBillett(Billett innBillett){
+        rep.lagreBillett(innBillett);
         }
-        else{
-            rep.lagreBillett(innBillett);
-        }
-    }
 
     @GetMapping("/hentAlle")
     public List<Billett> hentAlle(){
